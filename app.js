bf143144d9161f5b31a1279e8bbce1634854de23
Gabriel <tandohtakyie@hotmail.com>
2021-07-30 16:52:44 +0200

    Project startup setup

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Route
+app.use('/', require('./routes/index'));
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
+
